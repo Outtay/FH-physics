61174efc234f003fd14723b31d8cb60c359b631d
Florian Otti <otti.florian@gmail.com>
2018-03-29 18:00:22 +0200

    added phyiscs-project

---
+++ PhyicsUE01/.ycm_extra_conf.py
@@ -0,0 +1,4 @@
+def FlagsForFile( filename, **kwargs ):
+  return {
+    'flags': [ '-x', 'c++', '-std=c++11','-Wall', '-Wextra', '-Werror', '-isystem', '/home/xubuntu/Documents/FH/git/reactphysics3d/src' ],
+  }
